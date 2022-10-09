38561fb8d40a1094f397426c0e40d1b53d2e55b0
p92avav <114603272+p92avav@users.noreply.github.com>
2022-10-09 22:42:27 +0200

    Add files via upload

---
+++ Practice1/build/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/imc/MultilayerPerceptron.cpp.o"
+  "CMakeFiles/program.dir/imc/util.cpp.o"
+  "CMakeFiles/program.dir/la1.cpp.o"
+  "program"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
